80eaba036b2d2e13cd345e16d9932abe9b91b652
atst_96 <tnkjpei04+github@gmail.com>
2023-11-05 13:02:29 +0900

    add: 既存コードの追加

---
+++ Liberfy.UI.WPF.Sample/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Liberfy.UI.Wpf.Sample;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
